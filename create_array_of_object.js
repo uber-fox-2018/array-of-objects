d729897c3b2e664c416250bb0e696206072483e8
riadji.github.io <riadji.ramadhan@gmail.com>
2018-07-11 17:34:13 +0700

    Add Create Array of Object

---
+++ create_array_of_object.js
@@ -0,0 +1,30 @@
+var arr = [];
+
+function createObj (name, phase, gender) {
+  var result = [];
+  var obj = {
+    name : name,
+    phase : phase,
+    gender : gender,
+  };
+  arr.push(obj);
+}
+
+
+createObj('Akbar', 1, 'male');
+createObj('Icha', 1, 'female');
+createObj('Adhit', 2, 'male');
+createObj('Tama', 2, 'male');
+createObj('Rifky', 3, 'male');
+
+console.log(arr);
+
+function getData(name) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i]['name'] === 'Icha') {
+      return arr[i];
+    }
+  }
+}
+
+console.log(getData('Icha'));
